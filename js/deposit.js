a3f3584b9d9f60941170b2803bd45b72057e5f6b
susmoysaha6 <susmoysaha6@gmail.com>
2022-08-17 18:00:11 +0600

    deposit file changed using reusable function written in utility files

--- js/deposit.js
+++ js/deposit.js
@@ -1,25 +1,19 @@
-// step-1: add event listener to deposit button
 document.getElementById('btn-deposit').addEventListener('click', function () {
-    // get deposit amount 
-    const depositField = document.getElementById('deposit-field');
-    const newDepositAmountString = depositField.value;
-    const newDepositAmount = parseFloat(newDepositAmountString);
-    // /step-3 current deposit total
-    const depositTotalElement = document.getElementById('deposit-total');
-    const previousDepositTotalString = depositTotalElement.innerText;
-    const previousDepositTotal = parseFloat(previousDepositTotalString);
-    //step-4 add numbers
-    const currentDepositTotal = previousDepositTotal + newDepositAmount;
-    depositTotalElement.innerText = currentDepositTotal;
-    // step-5
-    const balanceTotalElement = document.getElementById('balance-total');
-    const previousBalanceTotalString = balanceTotalElement.innerText;
-    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
-    // step-6
-    const currentBalanceTotal = previousBalanceTotal + newDepositAmount;
-    // set the balance total 
-    balanceTotalElement.innerText = currentBalanceTotal;
+    const newDepositAmount = getInputFieldValueById('deposit-field');
 
-    // step-7 clear deposit feild
-    depositField.value = '';
-});
+    if (isNaN(newDepositAmount) || newDepositAmount < 0) {
+        alert('Please, enter a valid number');
+        return;
+    }
+
+    const previousDepositTotal = getTextElementValueById('deposit-total');
+    // 
+    const newDepositTotal = previousDepositTotal + newDepositAmount;
+    //
+    setTextElementValueById('deposit-total', newDepositTotal);
+    // previous balance 
+    const previousBalanceTotal = getTextElementValueById('balance-total');
+    const newBalanceTotal = previousBalanceTotal + newDepositAmount;
+    //
+    setTextElementValueById('balance-total', newBalanceTotal);
+})
